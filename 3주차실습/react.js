3ea1d8accf29aa04a5d14ba3c9f8b64b8b912c49
jisooshin <kelly6226@naver.com>
2023-05-11 14:27:52 +0900

    local storage 저장 및 삭제 기능 추가

--- 3주차실습/react.js
+++ 3주차실습/react.js
@@ -1,22 +1,63 @@
-const addList = document.getElementById("addList");
-const addButton = document.getElementById("addButton");
-const showList = document.getElementById("showList");
+const form = document.querySelector("form");
+const input = document.querySelector("input");
+const ul = document.querySelector("ul");
 
-addButton.addEventListener("click", function () {
-  const list = document.createElement("li");
-  if (!addList.value) {
+let todos = []; //배열 선언
+
+const save = () => {
+  localStorage.setItem("todos", JSON.stringify(todos));
+};
+
+const delItem = (event) => {
+  const target = event.target.parentElement;
+  todos = todos.filter((todo) => todo.id !== parseInt(target.id));
+  save();
+  target.remove();
+};
+
+const addItem = (todo) => {
+  if (!todo.text) {
     alert("추가될 할 일이 없습니다.");
   } else {
-    list.innerText = addList.value;
-    showList.appendChild(list);
-    addList.value = "";
+    const li = document.createElement("li");
+    const button = document.createElement("button");
+    const span = document.createElement("span");
+
+    span.innerText = todo.text;
+    button.innerText = "삭제";
+    button.addEventListener("click", delItem);
+
+    li.appendChild(span);
+    li.appendChild(button);
+    showList.appendChild(li);
+    li.id = todo.id;
   }
+};
 
-  list.addEventListener("dblclick", function () {
-    showList.removeChild(list);
-  });
+const handler = (event) => {
+  event.preventDefault();
 
-  list.addEventListener("click", function () {
-    list.style.textDecorationLine = "line-through";
-  });
-});
+  const todo = {
+    id: Date.now(),
+    text: input.value,
+  };
+
+  todos.push(todo);
+  addItem(todo);
+  save();
+  input.value = "";
+};
+
+const init = () => {
+  const userTodos = JSON.parse(localStorage.getItem("todos"));
+  if (userTodos) {
+    userTodos.forEach((todo) => {
+      addItem(todo);
+    });
+
+    todos = userTodos;
+  }
+};
+
+init();
+form.addEventListener("submit", handler);
